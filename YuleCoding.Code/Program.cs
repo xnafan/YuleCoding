418d39a241b02028122ee0340e09b61f9814668e
Jakob Farian Krarup <kr@rup.dk>
2024-11-14 13:25:35 +0100

    Changes to the exercises

--- YuleCoding.Code/Program.cs
+++ YuleCoding.Code/Program.cs
@@ -5,19 +5,28 @@
         static void Main(string[] args)
         {
 
-            YuleMachine yuleMachine = new();
+            YuleMachineSolved yuleMachine = new();
 
+            // Christmas Countdown
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor= ConsoleColor.White;
             for (int dayNumber = 0; dayNumber < 31; dayNumber++)
             {
                 Console.WriteLine(yuleMachine.ChristmasCounting(dayNumber));
             }
+            Console.WriteLine();
 
+
+            // Christmas Tree
             Console.ForegroundColor = ConsoleColor.Green;
-
             Console.WriteLine(yuleMachine.CreateChristmasTree(10));
-            Console.WriteLine(  );
+            Console.WriteLine();
+
+            //smaller tree
             Console.WriteLine(yuleMachine.CreateChristmasTree(3));
             Console.WriteLine();
+
+            //ASCII snow
             Console.BackgroundColor = ConsoleColor.DarkBlue;
             Console.ForegroundColor= ConsoleColor.White;
             Console.WriteLine(yuleMachine.CreateAsciiSnow(25,15, 3));
